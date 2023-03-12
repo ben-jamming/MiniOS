f492c8b19174638d765395423f64496e3acfa806
bhepdi <benjamin.hepditch@mail.mcgill.ca>
2023-03-12 00:37:12 -0500

    get my swagger back (undo fucked changes)

--- src/interpreter.c
+++ src/interpreter.c
@@ -10,6 +10,165 @@
 
 int MAX_ARGS_SIZE = 7;
 
+// Define the process states
+enum process_state {
+    READY,
+    RUNNING,
+    BLOCKED,
+    TERMINATED
+};
+
+// Define the process control block
+struct PCB {
+    int pid;                  // process ID
+    int program_location;     // address of first line
+    int program_counter;      // address of current line
+    int num_lines;            // number of lines in program
+    enum process_state state; // current process state
+    int priority;             // process scheduling priority
+    struct PCB *next;         // pointer to next PCB in ready queue
+};
+
+// Define the scheduling policy
+enum scheduling_policy {
+    FCFS, // First Come First Serve
+    // TODO: add more policies as needed
+};
+
+// Define the ready queue
+struct PCB *ready_queue_head = NULL;
+struct PCB *ready_queue_tail = NULL;
+
+// Helper function to display contents of PCB
+void print_pcb_contents(struct PCB *pcb){
+    printf("PID of this PCB: %d\n",pcb->pid);
+    printf("Address of the first line of program stored in this PCB: %d\n",pcb->program_location);
+    printf("Address of current line (program counter) of this PCB: %d\n",pcb->program_counter);
+    printf("Total number of lines in this PCB: %d\n", pcb->num_lines);
+    printf("Process state of this PCB: %d\n",pcb->state);
+    printf("Priority of this PCB: %d\n",pcb->priority);
+    if(pcb->next == NULL){
+        // Head of the queue
+        printf("This PCB is at the tail of the queue\n");
+    }
+    else{
+        printf("PID of next PCB: %d\n",pcb->next->pid);
+    }
+
+}
+
+// Helper function to print ready_queue
+void print_ready_queue(struct PCB *current_pcb) {
+    printf("Ready Queue: ");
+    while (current_pcb != NULL) {
+        printf("%d -> ", current_pcb->pid);
+        current_pcb = current_pcb->next;
+    }
+    printf("NULL\n");
+}
+
+
+// Add a new PCB to the tail of the ready queue
+void add_to_ready_queue(struct PCB *pcb) {
+    pcb->next = NULL;
+    if (ready_queue_head == NULL) {
+        // If the queue is empty, set both head and tail to the new PCB
+        ready_queue_head = pcb;
+        ready_queue_tail = pcb;
+    } else {
+        // Otherwise, add the new PCB to the tail of the queue
+        ready_queue_tail->next = pcb;
+        ready_queue_tail = pcb;
+    }
+}
+
+// Remove the head PCB from the ready queue and return it
+struct PCB *remove_from_ready_queue() {
+    if (ready_queue_head == NULL) {
+        // If the queue is empty, return NULL
+        return NULL;
+    } else {
+        // Otherwise, remove the head PCB and return it
+        struct PCB *removed_pcb = ready_queue_head;
+        ready_queue_head = ready_queue_head->next;
+        //if(removed_pcb != NULL){printf("PCB being removed is %d\n", removed_pcb->pid);}
+        //printf("Next head of the queue is %d\n",ready_queue_head->pid);
+        //print_ready_queue(ready_queue_head);
+        if (ready_queue_head == NULL) {
+            // If the queue is now empty, set the tail to NULL as well
+            ready_queue_tail = NULL;
+        }
+        return removed_pcb;
+    }
+}
+
+// Set up the PCB for a new process
+void setup_process(struct PCB *pcb, int pid, int program_location, int num_lines) {
+    // Set the PCB properties
+    // TODO:
+    // Configure the state and priority members to be dynamic
+    pcb->pid = pid;
+    pcb->program_location = program_location;
+    pcb->program_counter = program_location;
+    pcb->num_lines = num_lines;
+    pcb->state = READY;
+    pcb->priority = 0;
+    pcb->next = NULL;
+}
+
+// Run the program stored in a given PCB
+int run_program(struct PCB *pcb) {
+    char next_addr[4];
+    sprintf(next_addr, "%d", pcb->program_counter);  // Initialize next_addr to program_counter
+    // Error code
+    int errorCode = 0;
+    // Set the program counter to the address of the first line
+    int program_counter = pcb->program_location;
+    char line[1000];
+    // Loop over each line in the program
+    while (program_counter < pcb->num_lines+pcb->program_location) {
+        // Get the next line from memory
+        memset(line, '\0', sizeof(line));
+        sprintf(next_addr,"%d", program_counter);
+        strcpy(line, mem_get_value(next_addr));
+        //printf("The value retrieved from %s is: %s\n",next_addr,line);
+        // Interpret the line using parseInput
+        errorCode = parseInput(line);
+
+        // Increment the program counter to the next line
+        program_counter ++;
+
+        // Clear space for next line address
+        memset(next_addr, '\0', sizeof(next_addr));
+    }
+    // Free shell memory occupied by PCB
+    mem_reset(pcb->program_location, pcb->num_lines);
+    
+    return errorCode;
+}
+
+
+// Scheduler function
+void scheduler(enum scheduling_policy policy) {
+
+    // Run processes in the ready queue until there are no more processes
+    while (ready_queue_head != NULL) {
+        // Get the next process from the ready queue
+        struct PCB *next_pcb = remove_from_ready_queue();
+        //printf("PCB BEING RUN IN SCHEDULER IS: %d\n",next_pcb->pid);
+        //print_pcb_contents(next_pcb);
+        // Run the process
+        run_program(next_pcb);
+        // Clean up the process
+        // TODO:
+        // change ready_queue accordingly when a pcb is removed
+        free(next_pcb);
+        //print_pcb_contents(ready_queue_head);
+    }
+}
+
+
+
 int badcommand(){
 	printf("%s\n", "Unknown Command");
 	return 1;
@@ -55,8 +214,9 @@ int my_mkdir(char* dirName);
 int touch(char* filepath);
 char* subVar(char* token);
 int print(char* var);
-int run(char* script);
+int run(char** command_args, int arg_size);
 int badcommandFileDoesNotExist();
+int exec(char** command_args, int arg_size);
 
 char* get_all_but_first(char* str) {
     int str_len = strlen(str);
@@ -100,8 +260,10 @@ int interpreter(char* command_args[], int args_size){
 		return print(command_args[1]);
 	
 	} else if (strcmp(command_args[0], "run")==0) {
-		if (args_size != 2) return badcommand();
-		return run(command_args[1]);
+
+		if (args_size > 2) return badcommand();
+
+		return run(command_args,args_size);
 	
 	} else if (strcmp(command_args[0], "echo")==0){
 		if (args_size < 2) return badcommand();
@@ -131,7 +293,13 @@ int interpreter(char* command_args[], int args_size){
 
         touch(command_args[1]);
 
-    } else return badcommand();
+    } 
+    else if (strcmp(command_args[0], "exec")==0){
+        if (args_size < 5) return badcommandTooManyTokens();
+
+        exec(command_args,args_size);
+    }
+    else return badcommand();
 }
 
 int help(){
@@ -340,18 +508,14 @@ int set(char* var, char** value, int argsize){
     //loop through the argument pointers, starting at the first 
     for (int i=2;i<argsize;i++) {
         strcat(buffer, value[i]); //concatonate additional arguments
-
         //if this is not the last argument, add a space
         if (i<(argsize-1)) {
             strcat(buffer, link);
         }
     }
-
     //store in memory
 	mem_set_value(var, buffer);
-
 	return 0;
-
 }
 
 int print(char* var){
@@ -359,23 +523,70 @@ int print(char* var){
 	return 0;
 }
 
-int run(char* script){
+int run(char** command_args, int arg_size){
+
+    // TODO:
+    // Check validity of arguments passed into run
 	int errCode = 0;
-	char line[1000];
-	FILE *p = fopen(script,"rt");  // the program is in a file
+    // Copied implementation from set
+    char line[1000];
+    // Initialize start address var
+    int pcb_start_addr;
+    memset(line, '\0', 1000 * sizeof(line[0]));
+    //Loop over each program passed as argument
+    for (int arg = 1; arg < arg_size; arg++){
 
-	if(p == NULL){
-		return badcommandFileDoesNotExist();
-	}
+        FILE *p = fopen(command_args[arg],"rt");  // the program is in a file
+        //printf("argument %d: %s\n",arg,command_args[arg]);
+        if(p == NULL){
+            
+            return badcommandFileDoesNotExist();
+            
+        }
 
-	fgets(line,999,p);
-	while(1){
-		errCode = parseInput(line);	// which calls interpreter()
-		memset(line, 0, sizeof(line));
-		fgets(line,999,p);
-	}
+        // Create a new PCB
+        struct PCB *new_pcb = malloc(sizeof(struct PCB));
+        // Find space in memory to store program contents
+        pcb_start_addr = mem_find_first_free();
+        // Loop over each line of the program and save each into a different memory location
+        int line_num = 0;
+        // Address as a string (should be < 3 digits)
+        char str_addr[4];
+        while (fgets(line, 999, p)) {
+            // Calculate the address in memory for this line
+            int address = pcb_start_addr+line_num;
+            //printf("Address of memory slot for line number %d is: %d\n",line_num,address);
+            // Store the line in memory at this address
+            sprintf(str_addr,"%d",address);
+            //printf("line to be stored at %d is: %s\n",address, line);
+            mem_set_value(str_addr, line);
+            //printf("For comparison, the value retrieved from %s is: %s\n",str_addr,mem_get_value(str_addr));
+            // Move to the next line and reset address array and line buffer
+            line_num++;
+            memset(str_addr, '\0', sizeof(str_addr));
+            memset(line, '\0', sizeof(line));
+            
+        }
 
-    fclose(p);
+        //printf("----------Done loading the lines of this file---------\n");
+        // Configure the new PCB
+        setup_process(new_pcb, pcb_start_addr, pcb_start_addr, line_num);
+        //print_pcb_contents(new_pcb);
+        // Add the initial process to the ready queue
+        add_to_ready_queue(new_pcb);
+        
+        fclose(p);
+        }
+    //print_ready_queue(ready_queue_head);
+    // Run Scheduler (currently with FCFS policy)
+    //printf("----------------FINISHED LOADING PROGRAMS: RUNNING SCHEDULER----------------\n");
+    scheduler(FCFS);
 
 	return errCode;
 }
+
+int exec(char** command_args, int arg_size){
+    int errCode = 0;
+    return errCode;
+}
+
